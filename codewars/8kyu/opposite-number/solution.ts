356d24c487e4090605354b743080010ec9e79c8c
kimmanbo <hkim3139@gmail.com>
2021-09-13 22:10:11 +0900

    [codewars] 8kyu: Opposite number

---
+++ codewars/8kyu/opposite-number/solution.ts
@@ -0,0 +1,14 @@
+// My Solution
+export class Kata {
+  static opposite(n: number) {
+    return n * -1;
+  }
+}
+
+// Best Practice
+export class Kata {
+  static opposite(n: number) {
+    return -n;
+  }
+}
+
